4e69f375425bf15324d01cc5ef5a593b1b8f23dc
iago <iago.pferreiravr@gmail.com>
2022-02-01 12:11:06 -0300

    feat(tests): createUsers
    Implementando os teste dos 'casos de falha'

--- src/integration-tests/createUsers.test.js
+++ src/integration-tests/createUsers.test.js
@@ -26,7 +26,169 @@ describe('POST /users', () => {
   });
 
   describe('Casos de falha', () => {
-    
+    describe('Quando não é passado o campo "name"', () => {
+      let response;
+
+      before(async () => {
+        response = await chai.request(server)
+          .post('/users')
+          .send({
+            email: 'teste@teste.com',
+            password: '123456789'
+          });
+      });
+
+      it('retorna o código de status 400', () => {
+        expect(response).to.have.status(400);
+      });
+
+      it('retorna um objeto', () => {
+        expect(response).to.be.a('object');
+      });
+
+      it('o objeto possui a propriedade "message"', () => {
+        expect(response.body).to.have.property('message');
+      });
+
+      it('a propriedade "message" possui o texto "Invalid entries. Try again."', () => {
+        expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+      });
+    });
+
+    describe('Quando não é passado o campo "email"', () => {
+      let response;
+
+      before(async () => {
+        response = await chai.request(server)
+          .post('/users')
+          .send({
+            name: 'Testador Testante',
+            password: '123456789'
+          });
+      });
+
+      it('retorna o código de status 400', () => {
+        expect(response).to.have.status(400);
+      });
+
+      it('retorna um objeto', () => {
+        expect(response).to.be.a('object');
+      });
+
+      it('o objeto possui a propriedade "message"', () => {
+        expect(response.body).to.have.property('message');
+      });
+
+      it('a propriedade "message" possui o texto "Invalid entries. Try again."', () => {
+        expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+      });
+    });
+
+    describe('Quando não é passado um e-mail válido no campo "email"', () => {
+      let response;
+
+      before(async () => {
+        response = await chai.request(server)
+          .post('/users')
+          .send({
+            name: 'Testador Testante',
+            email: 'testatortestante@',
+            password: '123456789'
+          });
+      });
+
+      it('retorna o código de status 400', () => {
+        expect(response).to.have.status(400);
+      });
+
+      it('retorna um objeto', () => {
+        expect(response).to.be.a('object');
+      });
+
+      it('o objeto possui a propriedade "message"', () => {
+        expect(response.body).to.have.property('message');
+      });
+
+      it('a propriedade "message" possui o texto "Invalid entries. Try again."', () => {
+        expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+      });
+    });
+
+    describe('Quando não é passado o campo "password"', () => {
+      let response;
+
+      before(async () => {
+        response = await chai.request(server)
+          .post('/users')
+          .send({
+            name: 'Testador Testante',
+            email: 'testatortestante@',
+            password: '123456789'
+          });
+      });
+
+      it('retorna o código de status 400', () => {
+        expect(response).to.have.status(400);
+      });
+
+      it('retorna um objeto', () => {
+        expect(response).to.be.a('object');
+      });
+
+      it('o objeto possui a propriedade "message"', () => {
+        expect(response.body).to.have.property('message');
+      });
+
+      it('a propriedade "message" possui o texto "Invalid entries. Try again."', () => {
+        expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+      });
+    });
+
+    describe('Quando o e-mail passado não é único', () => {
+      let response;
+
+      before(async () => {
+        await chai.request(server)
+          .post('/users')
+          .send({
+            name: 'Testador Testante',
+            email: 'testatortestante@teste.com',
+            password: '123456789'
+          });
+
+        response = await chai.request(server)
+          .post('/users')
+          .send({
+            name: 'Testador Testante',
+            email: 'testatortestante@teste.com',
+            password: '123456789'
+          });
+      });
+
+      after(async () => {
+        db.collection('users').deleteMany({
+          name: 'Testador Testante',
+          email: 'testatortestante@teste.com',
+          password: '123456789'
+        })
+      })
+
+      it('retorna o código de status 409', () => {
+        expect(response).to.have.status(409);
+      });
+
+      it('retorna um objeto', () => {
+        expect(response).to.be.a('object');
+      });
+
+      it('o objeto possui a propriedade "message"', () => {
+        expect(response.body).to.have.property('message');
+      });
+
+      it('a propriedade "message" possui o texto "Invalid entries. Try again."', () => {
+        expect(response.body.message).to.be.equal('Email already registered');
+      });
+    });
   });
 
   describe('Casos de sucesso', () => {
