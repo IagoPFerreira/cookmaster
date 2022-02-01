968d3cbfaa0574bd1fbaf6b9266a2c18b906ced3
iago <iago.pferreiravr@gmail.com>
2022-02-01 16:47:59 -0300

    feat(users): userModel
    Implementando a funcao 'existingEmail' na camada model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,6 +2,15 @@ const connection = require('./connection');
 
 const coll = 'users';
 
+const existingEmail = async (email) => {
+  const exists = await connection()
+    .then((db) => db.collection(coll).findOne({ email }));
+
+  if (!exists) return null;
+
+  return exists;
+};
+
 const createUser = async (name, password, email, role) => {
   const newUser = await connection()
     .then((db) => db.collection(coll).insert({ name, password, email, role }))
@@ -14,5 +23,6 @@ const createUser = async (name, password, email, role) => {
 };
 
 module.exports = {
+  existingEmail,
   createUser,
 };
