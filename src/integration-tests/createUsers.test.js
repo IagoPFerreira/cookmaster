8f26f439de7cfb83a393ba2bee5cedd308ed7771
iago <iago.pferreiravr@gmail.com>
2022-02-01 11:34:21 -0300

    feat(tests): createUsers
    Implementacao base para os testes de falha e de sucesso
    
    Abertura e fechamento da conexao mockada com o banco

---
+++ src/integration-tests/createUsers.test.js
@@ -0,0 +1,35 @@
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+const sinon = require('sinon');
+const { MongoClient } = require('mongodb');
+
+const server = require('../api/app');
+const { getConnection } = require('./connectionMock');
+
+chai.use(chaiHTTP);
+
+const { expect } = chai;
+
+describe('POST /users', () => {
+  let db;
+
+  before(async () => {
+    const connectionMock = await getConnection();
+
+    db = connectionMock.db('Cookmaster');
+
+    sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+  });
+
+  after(async () => {
+    MongoClient.connect.restore();
+  });
+
+  describe('Casos de falha', () => {
+    
+  });
+
+  describe('Casos de sucesso', () => {
+    
+  });
+});
