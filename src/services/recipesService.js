2aa451deac8d2408d883d0ced26c9939bfce8a16
iago <iago.pferreiravr@gmail.com>
2022-02-02 14:18:53 -0300

    feat(recipes): recipesService
    Implementando a funcao 'getRecipe' na camada service

--- src/services/recipesService.js
+++ src/services/recipesService.js
@@ -12,7 +12,16 @@ const newRecipe = async ({ name, ingredients, preparation }) => {
 
 const getRecipes = async () => model.getRecipes();
 
+const getRecipe = async (id) => {
+  const recipe = await model.getRecipe(id);  
+
+  if (recipe === 'invalid id') return ({ status: 404, message: 'invalid id' });
+
+  return recipe;
+};
+
 module.exports = {
   newRecipe,
   getRecipes,
+  getRecipe,
 };
