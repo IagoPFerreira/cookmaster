8850137df3564f80d8279c773627e9e442c979c4
iago <iago.pferreiravr@gmail.com>
2022-02-02 12:59:08 -0300

    feat(recipes): recipesController
    Implementando a funcao 'getRecipes' na camada controller

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -9,6 +9,15 @@ const newRecipe = async (req, res) => {
   return res.status(201).json({ recipe });
 };
 
+const getRecipes = async (_req, res) => {
+  const recipe = await service.getRecipes();
+
+  if (recipe.length === 0) return res.status(404).json({ message: 'No registered recipe.' });
+
+  return res.status(200).json(recipe);
+};
+
 module.exports = {
   newRecipe,
+  getRecipes,
 };
