15c11ee0217f04a2d02b14a2cfc281650882a3fe
iago <iago.pferreiravr@gmail.com>
2022-02-18 11:45:27 -0300

    feat(recipes): recipesController
    Implementando a funcao 'editRecipe' na camada controller

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -20,11 +20,19 @@ const getRecipes = async (_req, res) => {
 const getRecipe = async (req, res) => {
   const recipe = await service.getRecipe(req.params.id);
 
-  if (recipe.message) return res.status(recipe.status).json({ message: recipe.message });
-
-  if (!recipe.name) {
+  if (!recipe) {
     return res.status(404).json({ message: 'recipe not found' });
   }
+
+  if (recipe.message) return res.status(recipe.status).json({ message: recipe.message });
+
+  return res.status(200).json(recipe);
+};
+
+const editRecipe = async (req, res) => {
+  const { params: { id }, body } = req;
+
+  const recipe = await service.editRecipe(id, body);
 
   return res.status(200).json(recipe);
 };
@@ -33,4 +41,5 @@ module.exports = {
   newRecipe,
   getRecipes,
   getRecipe,
+  editRecipe,
 };
