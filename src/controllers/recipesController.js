f01f3ba50c9a3a88fb5c98612bcb12036078b28d
iago <iago.pferreiravr@gmail.com>
2022-02-18 16:31:51 -0300

    feat(recipes): recipesController
    Implementando a funcao 'deleteRecipe' na camada controller

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -37,9 +37,30 @@ const editRecipe = async (req, res) => {
   return res.status(200).json(recipe);
 };
 
+const deleteRecipe = async (req, res) => {
+  const { params: { id }, user: { role } } = req;
+
+  if (role !== 'admin') {
+    return res.status(401).json({ message: 'only admins can delete recipes' });
+  }
+
+  const recipe = await service.getRecipe(id);
+
+  if (!recipe) {
+    return res.status(404).json({ message: 'recipe not found' });
+  }
+  
+  if (recipe.message) return res.status(recipe.status).json({ message: recipe.message });
+
+  await service.deleteRecipe(id);
+
+  return res.status(204).json({});
+};
+
 module.exports = {
   newRecipe,
   getRecipes,
   getRecipe,
   editRecipe,
+  deleteRecipe,
 };
